6d9e1193bc75ad9c6051ebd5e52bc4fe4147d9c0
Michael-Charara <chararam@outlook.com>
2021-02-25 12:42:52 -0500

    Update Program10.c

--- Programs/Program10.c
+++ Programs/Program10.c
@@ -11,12 +11,12 @@ typedef struct listNode
 typedef ListNode *ListNodePtr; // synonym for ListNode*
 
 // prototypes
+void instructions(void);                     // prototype for instructions function
 void insert(ListNodePtr *sPtr, char value);  // prototype for insert function
 char delete (ListNodePtr *sPtr, char value); // prototype for delete function
 char search(ListNodePtr *sPtr, char value);  // prototype for search function
 void printList(ListNodePtr currentPtr);      // prototype for print function
 int isEmpty(ListNodePtr sPtr);               // prototype for isempty function
-void instructions(void);                     // prototype for instructions function
 
 // main function
 int main(void)
@@ -26,83 +26,81 @@ int main(void)
 
    instructions(); // display the menu
    printf("%s", "? ");
-   unsigned int choice; // user's choice
-   scanf("%u", &choice);
+   unsigned int choice;  // user's choice
+   scanf("%u", &choice); // scan user input into unsigned int choice
 
-   // loop while user does not choose 3
+   // loop while user does not choose 5
    while (choice != 5)
    {
 
       switch (choice)
       {
-      case 1:
-         printf("%s", "Enter a character: ");
-         scanf("\n%c", &item);
-         insert(&startPtr, item); // insert item in list
-         printList(startPtr);
+      case 1:                                 // insert an element
+         printf("%s", "Enter a character: "); // prompt user for input
+         scanf("\n%c", &item);                // scan user input into char item
+         insert(&startPtr, item);             // insert item in list
+         printList(startPtr);                 // print list function
          break;
-      case 2: // delete an element
-         // if list is not empty
-         if (!isEmpty(startPtr))
+      case 2:                    // delete an element
+         if (!isEmpty(startPtr)) // if list is not empty
          {
-            printf("%s", "Enter character to be deleted: ");
-            scanf("\n%c", &item);
+            printf("%s", "Enter character to be deleted: "); // prompt user for input
+            scanf("\n%c", &item);                            // scan user input into char item
 
             // if character is found, remove it
             if (delete (&startPtr, item))
             { // remove item
                printf("%c deleted.\n", item);
-               printList(startPtr);
+               printList(startPtr); // print list
             }
-            else
+            else // if user input not found in list
             {
                printf("%c not found.\n\n", item);
             }
          }
-         else
+         else // if list is empty
          {
             puts("List is empty.\n");
          }
 
          break;
-      case 3: // search list
-         // if list is not empty
-         if (!isEmpty(startPtr))
+      case 3:                    // search list
+         if (!isEmpty(startPtr)) // if list is not empty
          {
-            printf("%s", "Enter character to search for: ");
-            scanf("\n%c", &item);
+            printf("%s", "Enter character to search for: "); // prompt user for input
+            scanf("\n%c", &item);                            // scan user input into char item
 
             // if character is found
             if (search(&startPtr, item))
-            { // remove item
+            {
                printf("%c found.\n", item);
-               printList(startPtr);
+               printList(startPtr); // print list
             }
-            else
+            else // if character not found
             {
                printf("%c not found.\n\n", item);
             }
          }
-         else
+         else // if list is empty
          {
-            puts("List is empty.\n");
+            puts("List is empty.\n"); // inform user that list is empty
          }
 
          break;
-      case 4: // print list
-         printList(startPtr);
+      case 4:                 // print list
+         printList(startPtr); // print list function
          break;
-      default:
-         puts("Invalid choice.\n");
-         instructions();
+      default:                      // default case
+         puts("Invalid choice.\n"); // inform user of invalid choice
+         instructions();            // instructions function
          break;
       } // end switch
 
-      printf("%s", "? ");
-      scanf("%u", &choice);
+      printf("%s", "? ");   // prompt user for next input
+      scanf("%u", &choice); // scan user input into int choice
    }
 
-   puts("End of run.");
+   puts("End of run."); // inform user of program end
 }
 
 // display program instructions to user
@@ -190,37 +188,7 @@ char delete (ListNodePtr *sPtr, char value)
    return '\0';
 }
 
-// return 1 if the list is empty, 0 otherwise
-int isEmpty(ListNodePtr sPtr)
-{
-   return sPtr == NULL;
-}
-
-// print the list
-void printList(ListNodePtr currentPtr)
-{
-   // if list is empty
-   if (isEmpty(currentPtr))
-   {
-      puts("List is empty.\n");
-   }
-   else
-   {
-      puts("The list is:");
-
-      // while not the end of the list
-      while (currentPtr != NULL)
-      {
-         printf("%c --> ", currentPtr->data);
-         currentPtr = currentPtr->nextPtr;
-      }
-
-      puts("NULL\n");
-   }
-}
-
-/* Checks whether the value x is present in linked list */
-// delete a list element
+// search for a list element
 char search(ListNodePtr *sPtr, char value)
 {
    // delete first node if a match is found
@@ -252,3 +220,44 @@ char search(ListNodePtr *sPtr, char value)
 
    return '\0';
 }
+
+// print the list
+void printList(ListNodePtr currentPtr)
+{
+   // if list is empty
+   if (isEmpty(currentPtr))
+   {
+      puts("List is empty.\n");
+   }
+   else
+   {
+      puts("The list is:");
+
+      // while not the end of the list
+      while (currentPtr != NULL)
+      {
+         printf("%c --> ", currentPtr->data);
+         currentPtr = currentPtr->nextPtr;
+      }
+
+      puts("NULL\n");
+   }
+}
+
+// return 1 if the list is empty, 0 otherwise
+int isEmpty(ListNodePtr sPtr)
+{
+   return sPtr == NULL;
+}
+
+/*
+Resources:
+Deitel, Paul J., and Harvey M. Deitel. C: How to Program. Pearson, 2016. 
+fig 12.03.c and fig 12.13.c
+Search an element in a Linked List (Iterative and Recursive):
+https://www.geeksforgeeks.org/search-an-element-in-a-linked-list-iterative-and-recursive/
+C program to search an element in linked list:
+https://codeforwin.org/2018/05/c-program-to-search-an-element-in-linked-list.html
+Linked List | Set 1 (Introduction)
+https://www.geeksforgeeks.org/linked-list-set-1-introduction/
+*/
